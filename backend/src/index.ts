a540c469a1041b2aa66438a5c9b8bc9b4b362054
Tanishq <tanishq162005@gmail.com>
2024-10-02 14:28:07 +0530

    basic

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+
+// Create a new express application
+const app = express();
+app.use(express.json());
+app.use(cors());
+// Define a route to handle GET requests at the root path ("/")
+app.get("/", (req, res) => {
+  // Send a JSON response with the message "Hello, World!"
+  res.json({ message: "Hello, World!" });
+});
+
+
+// Define a route to handle POST requests at the "/api/echo" path
