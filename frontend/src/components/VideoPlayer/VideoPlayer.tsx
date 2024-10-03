6c0937fa96d0594387358168c01ffa5a9274702a
Ujjawal179 <ujjawal.agrawal179@gmail.com>
2024-10-03 23:26:58 +0530

    minor changes testing

--- frontend/src/components/VideoPlayer/VideoPlayer.tsx
+++ frontend/src/components/VideoPlayer/VideoPlayer.tsx
@@ -1,43 +1,47 @@
 import React, { useEffect } from 'react';
 
 interface VideoPlayerProps {
-    size: number;
-    position: number;
     video: string;
-    time: number;
 }
+// interface VideoPlayerProps {
+//     size: number;
+//     position: number;
+//     video: string;
+//     time: number;
+// }
 
-const VideoPlayer: React.FC<VideoPlayerProps> = ({ size, position, video, time }) => {
+const VideoPlayer: React.FC<VideoPlayerProps> = ({  video }) => {
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            const videoElement = document.querySelector('.image-player');
-            if (videoElement) {
-                videoElement.remove();
-            }
-        }, time * 1000);
+    // useEffect(() => {
+    //     const timer = setTimeout(() => {
+    //         const videoElement = document.querySelector('.image-player');
+    //         if (videoElement) {
+    //             videoElement.remove();
+    //         }
+    //     }, time * 1000);
 
-        return () => clearTimeout(timer);
-    }, [time]);
+    //     return () => clearTimeout(timer);
+    // }, [time]);
 
-    if (!video) {
-        return null;
-    }
-    if( size < 1) size = 1;
+    // if (!video) {
+    //     return null;
+    // }
+    // if( size < 1) size = 1;
 
-    const calculatedSize = 100 / size;
-    const top = (position / size) * 100;
-    const left = (position % size) * 100;
-    const style: React.CSSProperties = {
-        width: calculatedSize + '%',
-        height: calculatedSize + '%',
-        top: top + '%',
-        left: left + '%',
-        position: 'absolute'
-    };
+    // const calculatedSize = 100 / size;
+    // const top = (position / size) * 100;
+    // const left = (position % size) * 100;
+    // const style: React.CSSProperties = {
+    //     width: calculatedSize + '%',
+    //     height: calculatedSize + '%',
+    //     top: top + '%',
+    //     left: left + '%',
+    //     position: 'absolute'
+    // };
 
     return (
-        <div className="image-player" style={style}>
+        // <div className="image-player" style={style}>
+        <div className="image-player" >
             <video
                 src={video}
                 style={{ width: '100%', height: '100%' }}
