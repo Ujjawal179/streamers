2978c9827397e17f7937a8bdf26e7e8fb2227335
Ujjawal179 <ujjawal.agrawal179@gmail.com>
2024-10-24 20:37:24 +0530

    update profile

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { TextField, InputLabel, FormControl, NativeSelect, Box, Button } from "@mui/material";
 import styles from "./Profile.module.css";
+import { updateUser } from "../../api/userService";
 
 const Profile: React.FC = () => {
   const [Name, setName] = useState<string>("");
   const [phoneNumber, setPhoneNumber] = useState<number | string>();
-  const [about, setAbout] = useState< string>("");
+  // const [about, setAbout] = useState< string>("");
   const [channelLinks, setChannelLinks] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
 
   // const addChannelLink = () => {
   //   setChannelLinks([...channelLinks, ""]);
@@ -22,17 +24,20 @@ const Profile: React.FC = () => {
     setChannelLinks(e.target.value);
   }
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const profileData = {
-      Name,
-      phoneNumber,
-      about,
-      channelLinks,
-    };
-    console.log(profileData);
-    // You can add further logic to handle the form submission, such as sending the data to a server
-  };
+    try {
+      const response = await updateUser({ Name, phoneNumber, channelLinks });
+      if (response.status === 200) {
+        setError("Details updated successfully");
+          } else {
+            setError(`Error updating details`);
+          }
+
+      } catch (error) {
+        setError("Error updating details");
+      }
+    }
 
   return (
     <div>
@@ -40,13 +45,14 @@ const Profile: React.FC = () => {
       <TextField variant="standard" required label="Name" type="text" name="name" value={Name} onChange={(e) => setName(e.target.value)} />
       <TextField variant="standard" label="Phone Number (with country code)" type="text" name="phoneNum" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
       {/* <TextField variant="standard" required label="About" type="textarea" multiline name="about" value={about || ''} onChange={(e) => setAbout(e.target.value)} /> */}
-      <TextField variant="standard" required label="Channel Link" type="text" name="mandatoryChannelLink" value={channelLinks[0] || ''} onChange={(e) => handleChannelLinkChange(e)} />
+      <TextField variant="standard" required label="Channel Link" type="text" name="channelLink" value={channelLinks || ''} onChange={(e) => handleChannelLinkChange(e)} />
       {/* {channelLinks.slice(1).map((link, index) => (
       <TextField variant="standard" key={index + 1} label={`Channel Link ${index + 2}`} type="text" value={link} onChange={(e) => handleChannelLinkChange(e, index + 1)} />
       ))}
       {channelLinks.length < 3 && (
       <Button onClick={addChannelLink}>Add Channel Link</Button>
       )} */}
+      <p style={{ color: error === "Details updated successfully" ? 'blue' : 'red' }}>{error}</p>
       <Button type="submit">Submit</Button>
     </form>
     </div>
